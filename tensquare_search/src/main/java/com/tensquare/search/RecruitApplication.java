f83dc4461695ea634679a657b735010f6c742d4d
lisi <1786678583@qq.com>
2020-02-22 01:23:15 +0800

    第三天结束

---
+++ tensquare_search/src/main/java/com/tensquare/search/RecruitApplication.java
@@ -0,0 +1,19 @@
+package com.tensquare.search;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import util.IdWorker;
+
+@SpringBootApplication
+public class RecruitApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RecruitApplication.class);
+    }
+
+    @Bean
+    public IdWorker idWorkker(){
+        return new IdWorker(1, 1);
+    }
+}
---
+++ tensquare_search/src/main/java/com/tensquare/search/pojo/Article.java
@@ -0,0 +1,20 @@
+package com.tensquare.search.pojo;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+
+//indexName是索引库（相当于数据库），type是类型（相当于表）
+@Document(indexName = "tensquare",type = "article")
+public class Article {
+    @Id
+    private String id;//ID
+    //是否索引，就是看该域是否能被搜索
+    //是否分词，就表示搜索的时候是整体匹配还是单词匹配
+    //是否存储，就是是否在页面上显示
+    //比如整个文章内容是：索引（可以搜着），不分词（整偏文章才能搜着），不存储（因为不在页面显示）
+    //titile:索引，分词，存储
+    @Field()
+    private String title;
+
+}
